38baf7f7ce1eb1df8a3e5f004646cad81a1c7bc2
chuotbeotester <chuot.beo.di.tester@gmail.com>
2025-09-26 22:10:19 +0700

    feat: add folder homework

---
+++ hai-chuot/BT_Buoi4/bai_tap3.py
@@ -0,0 +1,20 @@
+# NOTE BÀI 3: Viết chương trình cắt chuỗi:
+# s = "Automation Testing" 
+# Lấy ra từ "Automation".
+# Lấy ra từ "Testing".
+# Lấy 5 ký tự đầu tiên.
+# Lấy 3 ký tự cuối cùng.
+
+text = "Automation Testing" 
+
+# Lấy ra từ "Automation".
+print(text[:10])
+
+# Lấy ra từ "Testing".
+print(text[11:])
+
+# Lấy 5 ký tự đầu tiên.
+print(text[:5])
+
+# Lấy 3 ký tự cuối cùng.
+print(text[-3:])
---
+++ hai-chuot/BT_Buoi4/bai_tap5.py
@@ -0,0 +1,9 @@
+# NOTE BÀI 5: Cho list:
+# fruits = ["apple", "banana", "cherry"]
+
+# Dùng join() để nối list thành một chuỗi dạng: "apple, banana, cherry".
+
+fruits = ["apple", "banana", "cherry"]
+text = ", ".join(fruits)
+
+print(f"Chuỗi mong đợi: {text}")
