bdb9374bb1f5a3efdff6d6584b093cb9195a9a69
Khang Nguyen <mkhang0995.it@gmail.com>
2025-10-04 16:32:54 +0700

    Bai tap OOP cua Khang

---
+++ BaiTap_Khang/BT_Buoi8_OOP/calculator.py
@@ -0,0 +1,34 @@
+# Máy tính đơn giản Viết một class Calculator.
+# Không cần __init__ phức tạp.
+# Phương thức add(a, b): Trả về tổng của a và b.
+# Phương thức subtract(a, b): Trả về hiệu của a và b.
+# Phương thức multiply(a, b): Trả về tích của a và b.
+# Phương thức divide(a, b): Trả về thương của a và b. Dùng try-except để xử lý trường hợp b bằng 0.
+
+class Calculator:
+    def add(self, a, b):
+        d1 = a + b
+        print(f"{a} + {b} = {d1}")
+    def subtract(self, a, b):
+        d2 = a - b
+        print(f"{a} - {b} = {d2}")
+    def multiply(self, a, b):
+        d3 = a * b
+        print(f"{a} * {b} = {d3}")
+    def divide(self, a, b):
+        try:
+            d4 = a / b
+            print(f"{a} / {b} = {d4}")
+            # return d4
+        except ZeroDivisionError:
+            print("Error: Division by zero is invalid")
+        
+calc = Calculator()        
+calc.add(4, 2)
+calc.subtract(4, 2)
+calc.multiply(4, 2)
+calc.divide(4, 2)
+calc.divide(4, 0)
+
+
+    
