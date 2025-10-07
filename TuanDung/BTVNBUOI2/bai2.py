467fa480ac93fc25b1eb0e6e62e63435f08d03a2
Your Name <you@example.com>
2025-10-07 18:16:23 +0700

    Update bt

--- TuanDung/BTVNBUOI2/bai2.py
+++
@@ -1,4 +0,0 @@
-#tạo Dict
-newDict = {"name" : "Lan", "age" : 25, "city" : "Hà Nội"}
-#in ra màn hình các phần tử
-print(newDict)
--- TuanDung/BTVNBUOI4/bai4.py
+++
@@ -1,5 +0,0 @@
-messy_phone = " 090-123 4567 "
-
-phoneNumber = messy_phone.replace(" ", "").replace("-", "")
-
-print("SĐT chuẩn hóa: ", phoneNumber)
