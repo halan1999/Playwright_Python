a02ede767f2a0a0e6f627abe3aef380ad571d237
Khang Nguyen <mkhang0995.it@gmail.com>
2025-10-07 15:05:59 +0700

    OOP exercise, full, Khang

--- BaiTap_Khang/BT_OOP_Part2/bt1.py
+++ BaiTap_Khang/BT_OOP_Part2/bt1.py
@@ -12,7 +12,6 @@ from abc import ABC, abstractmethod
 class BaseElement(ABC):
     def __init__(self, locator):
         self.__locator = locator
-        
     def get_locator(self):
         return self.__locator
 
